807c7660635bd2486f8f009faaff3d92c2d18eb8
Jack W. <git@jack.cab>
2024-06-24 16:22:39 -0400

    feat: :sparkles: Redirect generator

---
+++ generate.mjs
@@ -0,0 +1,15 @@
+import { writeFile, readFile, mkdir } from "node:fs/promises"
+
+const dist = mkdir("dist", { recursive: true })
+const sites_txt = await readFile("sites.txt", "utf-8")
+const sites = sites_txt.split("\n").filter(Boolean)
+
+const redirects = sites
+	.flatMap((site, index) => [
+		`/prev/${site} ${sites.at(index - 1)} 301`,
+		`/next/${site} ${sites[(index + 1) % sites.length]} 301`,
+	])
+	.join("\n")
+
+await dist
+writeFile("dist/_redirects", redirects)
